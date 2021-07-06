3dac9aea1eabad5543dadce35007158ebb487dca
CamGitH <47019927+CamGitH@users.noreply.github.com>
2021-07-06 14:04:03 -0500

    Update Tarea4.java

--- src/Tarea4.java
+++ src/Tarea4.java
@@ -45,9 +45,98 @@ public class Tarea4 {
 	}
 	
 	public String hallarNMasRepetido(ArrayList<String> a, ArrayList<String> b) {
-		String r ="";
-		
-		
-		return r;
+			import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class tarea {
+    public static void main(String[] args) {
+
+		    //n = largo del array 
+		    //a = array 1
+		    //b - array 2 
+
+		    int n = 4;
+
+		    Int a[] = a[1,2,3,4]; 
+		    Int b[] = new ArrayList<>(); 
+		    Int b1[] = new ArrayList<>(); 
+		    Int a1[] = new ArrayList<>(); 
+
+
+		    int cont = 1 ;
+		    int max = 0 ;
+		    int actual = 0;
+		    int i = 0;
+		    int j = 0;
+		    int j1 = 0;
+
+		    mergeSort(a, n);
+		    mergeSort(b, n);
+
+
+		    while(i < n && j < n ){
+			if(a[1]==b[j] && b1[j1] != a[i]){
+			    b1[j1] = b[j];
+			    j++;
+			    j1++;
+			}
+			else if(a[i]<b[j]){
+			    i++;
+			}
+			else if(){
+
+			}
+		    }
+
+
+		    System.out.println(cont);   
+
+
+		    }
+
+
+		    public static void mergeSort(int[] a, int n) {
+			if (n < 2) {
+			    return;
+			}
+			int mid = n / 2;
+			int[] l = new int[mid];
+			int[] r = new int[n - mid];
+
+			for (int i = 0; i < mid; i++) {
+			    l[i] = a[i];
+			}
+			for (int i = mid; i < n; i++) {
+			    r[i - mid] = a[i];
+			}
+			mergeSort(l, mid);
+			mergeSort(r, n - mid);
+
+			merge(a, l, r, mid, n - mid);
+		    }
+
+		    public static void merge(
+		  int[] a, int[] l, int[] r, int left, int right) {
+
+		    int i = 0, j = 0, k = 0;
+		    while (i < left && j < right) {
+			if (l[i] <= r[j]) {
+			    a[k++] = l[i++];
+			}
+			else {
+			    a[k++] = r[j++];
+			}
+		    }
+		    while (i < left) {
+			a[k++] = l[i++];
+		    }
+		    while (j < right) {
+			a[k++] = r[j++];
+		    }
+		}
+
+
+		}
+
 	}
 }
